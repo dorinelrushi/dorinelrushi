e44722900eae16f1d6b1c25334608d148b9fbb8b
dorinel.js <38764706+dorinelrushi@users.noreply.github.com>
2024-07-21 21:01:33 +0000

    last login

---
+++ src/app/About/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About  () {
+  return (
+    <div className='w-[80%] m-auto'>
+     
+    </div>
+  )
+}
+
+export default About
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header/Header";
 import { Analytics } from "@vercel/analytics/react"
-import Head from "next/head";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-     
-      </Head>
+   
       <body className={inter.className}>
         <div className="">
           <Header />
