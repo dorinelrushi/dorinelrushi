cea43447709b8f8079181bfe527479bd145bb6cf
dorinel.js <38764706+dorinelrushi@users.noreply.github.com>
2024-07-26 23:15:41 +0000

    latest

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header/Header";
 import { Analytics } from "@vercel/analytics/react"
+import Script from 'next/script';
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-   
+      <Script async type="text/javascript" src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=XWXiHp" />
       <body className={inter.className}>
         <div className="">
           <Header />
